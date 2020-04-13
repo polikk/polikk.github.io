c9506fd37e9a47ff3173b32f1f8408750e8a8a51
polikk <polikigor@ukr.net>
2020-04-13 23:03:21 +0300

    new cite

---
+++ apolon/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$('.main-info').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+$('.comunity-part').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  adaptiveHeight: true
+});
