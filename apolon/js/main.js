f9c5c1dab4a841d341e2c36d05b342731588efd1
polikk <polikigor@ukr.net>
2020-04-18 01:10:39 +0300

    New version

--- apolon/js/main.js
+++ apolon/js/main.js
@@ -4,12 +4,16 @@ $('.main-info').slick({
   dots: true,
   infinite: true,
   speed: 300,
+  autoplay: true,
+  autoplaySpeed: 3000,
   slidesToShow: 1,
   adaptiveHeight: true
 });
 $('.comunity-part').slick({
   dots: false,
   infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
   speed: 300,
   slidesToShow: 3,
   adaptiveHeight: true
