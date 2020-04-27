316e12b53df679399db166a7b4c445da51d5f49e
polikk <polikigor@ukr.net>
2020-04-27 23:03:54 +0300

    1

--- apolon/js/main.js
+++ apolon/js/main.js
@@ -18,3 +18,28 @@ $('.comunity-part').slick({
   slidesToShow: 3,
   adaptiveHeight: true
 });
+$('.press-slider').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 300,
+  slidesToShow: 3,
+  adaptiveHeight: true
+});
+$('.famuous-slider').slick({
+  centerMode: true,
+  centerPadding: '10px',
+  slidesToShow: 3,
+  //infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  adaptiveHeight: true
+});
+var $status = $('.pagingInfo');
+var $slickElement = $('.famuous-slider');
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
+});
